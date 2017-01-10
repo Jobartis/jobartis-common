d0e0f60c31b0608f81476f9b1713af9df1b41bf8
Maciej Lotkowski <maciej.lotkowski@gmail.com>
2017-01-10 14:40:31 +0100

    Initial commit to jobartis-common.

---
+++ test/helper.rb
@@ -0,0 +1,26 @@
+require 'simplecov'
+
+module SimpleCov::Configuration
+  def clean_filters
+    @filters = []
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_adapter 'test_frameworks'
+end
+
+ENV["COVERAGE"] && SimpleCov.start do
+  add_filter "/.rvm/"
+end
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'jobartis-common'
+
+class Test::Unit::TestCase
+end
