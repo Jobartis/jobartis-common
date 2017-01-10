408139560c20b8074fc97669e3410cc61bdf87c9
Maciej Lotkowski <maciej.lotkowski@gmail.com>
2017-01-10 17:27:23 +0100

    Initial commit

---
+++ lib/jobartis-common.rb
@@ -0,0 +1,39 @@
+module JobartisCommon
+  module Contact
+    module Candidates
+      def email
+        Email.new "support@jobartis.com", "Jobartis Support"
+      end
+
+      def phone
+        Phone.new "996790419"
+      end
+    end
+
+    module Companies
+      def email
+        Email.new "info@jobartis.com", "Jobartis Support"
+      end
+
+      def phone
+        Phone.new "929659742"
+      end
+    end
+
+    Phone = Struct.new(:number) do
+      def pretty
+        "(+244) #{ number.scan(/.{1,3}/).join(" ") }"
+      end
+
+      alias :to_s :number
+    end
+
+    Email = Struct.new(:address, :name) do
+      def pretty
+        "#{ name } <#{ address }>"
+      end
+
+      alias :to_s :address
+    end
+  end
+end
--- test/test_jobartis-common.rb
+++ test/test_jobartis-common.rb
@@ -1,7 +1,16 @@
 require 'helper'
 
 class TestJobartisCommon < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_format_email_address
+    email = JobartisCommon::Contact::Email.new("maciej.lotkowski@jobartis.com", "Maciej Lotkowski")
+
+    assert_equal "maciej.lotkowski@jobartis.com", "#{email}"
+    assert_equal "Maciej Lotkowski <maciej.lotkowski@jobartis.com>", email.pretty
+  end
+
+  def test_format_phone_number
+    phone = JobartisCommon::Contact::Phone.new("897174783")
+
+    assert_equal "897174783", "#{phone}"
   end
 end
