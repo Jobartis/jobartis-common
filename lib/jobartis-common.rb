a62b9e1ff098032372e846e538c15ce74e69ba92
Marek Lotkowski <marek.lotkowski@gmail.com>
2019-01-18 16:25:08 +0100

    Updated gem: JobartisCommon::Contact::Company#phone method required country_code string argument now

--- lib/jobartis-common.rb
+++ lib/jobartis-common.rb
@@ -1,5 +1,19 @@
 module JobartisCommon
   module Contact
+    PREFIX_FOR = {
+      "AO" => "+244",
+      "ZM" => "+260",
+      "CM" => "+237"
+    }
+
+    PHONE_FOR_COMPANY_FROM = {
+      "AO" => "225300180",
+      "ZM" => "973332926",
+      "CM" => "697281166"
+    }
+
+
+
     module Candidate
       def email(tld = 'com')
         Email.new "info@jobartis.#{tld}", "Jobartis Support"
@@ -17,16 +31,18 @@ module JobartisCommon
         Email.new "support@jobartis.#{tld}", "Jobartis Support"
       end
 
-      def phone
-        Phone.new "225300180"
+      def phone(country)
+        code = country,code
+        Phone.new JobartisCommon::Contact::PHONE_FOR_COMPANY_FROM[country.code],
+                  JobartisCommon::Contact::PREFIX_FOR[country.code]
       end
 
       extend self
     end
 
-    Phone = Struct.new(:number) do
+    Phone = Struct.new(:number, :prefix) do
       def pretty
-        "(+244) #{ number.scan(/.{1,3}/).join(" ") }"
+        "(#{prefix}) #{ number.scan(/.{1,3}/).join(" ") }"
       end
 
       alias :to_s :number
